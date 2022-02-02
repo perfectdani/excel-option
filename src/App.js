d44b814a133e39cb8960baff0fb139d42caed6a3
perfectdani <ooodeve@gmail.com>
2022-02-02 14:44:41 +0200

    download window

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react';
 import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RangeDirective, getRangeAddress } from '@syncfusion/ej2-react-spreadsheet';
 import { Input, Select, Button, Modal, notification } from 'antd';
-import { LineChartOutlined } from '@ant-design/icons'
+import { LineChartOutlined,SaveOutlined } from '@ant-design/icons'
 
 import './App.css'
 
@@ -39,9 +39,11 @@ function App() {
       }
     }
     setColumns(colsArr);
+
   }
 
-  const percentApply = () => {
+  function percentApply() {
+    console.log(ssRef);
     if (applyType && applyPercent > 0 && applyColumns.length) {
       setModalVisible(false);
       setApplyType(null);
@@ -52,28 +54,40 @@ function App() {
       let arr = [];
       let row = [];
       let index = 0;
+      let current_row = 0; 
       ssRef.current.getData(getRangeAddress([0, 0, usedRowIdx, usedColIdx])).then((cells) => {
         cells.forEach((cell, key) => {
           if (index > usedColIdx) {
             arr = [...arr, row];
             row = [];
             index = 0;
+            current_row++;
           }
           let newVlaue = cell.value;
-          if (newVlaue && applyColumns.includes(String(index))) {
-            if (applyType == 1) {
+          if (newVlaue &&newVlaue!==undefined&& applyColumns.includes(String(index))) {
+            
+            if (applyType === 1) {
               newVlaue = newVlaue * applyPercent / 100;
-            } else if (applyType == 2) {
+            } else if (applyType === 2) {
               newVlaue = newVlaue * (1 + applyPercent / 100);
             } else {
               newVlaue = newVlaue * (1 - applyPercent / 100);
             }
             newVlaue = Math.ceil(newVlaue);
+            if(!isNaN(newVlaue))
+              ssRef.current.sheets[0].rows[current_row].cells[index].value = newVlaue;
           }
           row = [...row, newVlaue];
           index++;
         })
         setData(arr);
+        console.log(arr);
+        // ssRef.current.sheets[0].ranges[0].dataSource = arr;
+        
+        
+      }).then(()=>{
+        ssRef.current.refresh(false)
+        // ssRef.current.sheets[0].ranges[0].dataSource = arr;
       });
     } else {
       notification.warning({
@@ -88,10 +102,76 @@ function App() {
     setApplyPercent(null);
     setApplyColumns([]);
   }
+  
+  const save = (ext) =>{
+    let usedRowIdx = ssRef.current.getActiveSheet().usedRange.rowIndex;
+    let usedColIdx = ssRef.current.getActiveSheet().usedRange.colIndex;
+    let index = 0;
+    let dollarUSLocale = Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    
+      // These options are needed to round to whole numbers if that's what you want.
+      //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+      //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+    ssRef.current.getData(getRangeAddress([0, 0, usedRowIdx, usedColIdx])).then((cells) => {
+      console.log(cells);
+      let txtData = "";
+      cells.forEach((cell,key)=>{
+        if (index > usedColIdx) {
+          index = 0;
+          txtData += "\r\n";
+        }
+        let newValue = cell.value;
+        console.log(cell.format);
+        
+        if(cell.format!==undefined){
+          newValue = dollarUSLocale.format(newValue);
+          newValue=`"`+newValue+`"`;
+          console.log(newValue);
+        }
 
+        if(newValue===undefined)
+          txtData += ",";
+        else
+          txtData += newValue+",";
+        index++;
+      });
+      const filename = "sample."+ext;
+      download(filename,txtData);
+    }).then(()=>{
+      
+      // ssRef.current.sheets[0].ranges[0].dataSource = arr;
+    });
+  }
+  async function download(filename, text) {
+    const image = new Blob([text], { type: 'application/octet-binary' })
+    const opts = {
+      types: [{
+        description: 'CSV file',
+        accept: {'text/csv': ['.csv']},
+      }],
+    };
+    if( window.showSaveFilePicker ) {
+      const handle = await window.showSaveFilePicker(opts);
+      const writable = await handle.createWritable();
+      await writable.write( image );
+      writable.close();
+    }
+    else {
+      const saveImg = document.createElement( "a" );
+      saveImg.href = URL.createObjectURL( image );
+      saveImg.download= "image.png";
+      saveImg.click();
+      setTimeout(() => URL.revokeObjectURL( saveImg.href ), 60000 );
+    }
+  }
+  
   return (
     <div className='App'>
       <Button type="text" icon={<LineChartOutlined />} id="modal-button" onClick={beforeModal} />
+      <Button type="text" icon={<SaveOutlined />} id="save_csv" onClick={()=>save('csv')} />
       <Modal
         style={{ top: 200 }}
         visible={modalVisible}
@@ -119,6 +199,7 @@ function App() {
         showSheetTabs={false}
         ref={ssRef}
         beforeOpen={beforeOpen}
+        allowSave= {false}
         openUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open'
         saveUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'
       >
